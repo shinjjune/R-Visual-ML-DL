dfc07d6be01abf1e9c6f63fcc96735158c3139a7
shinjjune <sjh8236@naver.com>
2019-08-31 23:47:15 +0900

    update?

--- test.R
+++ test.R
@@ -2,11 +2,15 @@ install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("ggthemes")
 
-library(dplyr)
-library(ggplot2)
-library(ggthemes)
+library("dplyr")
+library("ggplot2")
+library("ggthemes")
 
 DF <- read.csv("example_population_f.csv")
-DF <- DF[,1]
-DF <-tbl_df(DF)
+DF <- DF[,-1]
+DF <- tbl_df(DF)
 DF
+
+DF2 <- filter(DF,Provinces=="충청북도"|Provinces=="충청남도")
+DF2
+
