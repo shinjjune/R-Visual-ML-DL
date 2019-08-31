c4531b33d3708c7b740f2fab1408c667a196c511
shin june-hyuk <47058441+shinjjune@users.noreply.github.com>
2019-08-31 23:55:35 +0900

    Update test.R

--- test.R
+++ test.R
@@ -25,4 +25,3 @@ Graph <- ggplot(DF3,aes(City,y=SexRatio,fill=Provinces))+geom_bar(stat="identity
 Graph
 
 
-![image](https://user-images.githubusercontent.com/47058441/64065628-c23dff80-cc4a-11e9-9cc5-75d6a6c693b8.png)
