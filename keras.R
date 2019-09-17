afde0c320ffcc8da47c49435edfb76f65def2b06
shinjune <47058441+shinjjune@users.noreply.github.com>
2019-09-17 23:56:24 +0900

    Update and rename keras to keras.R

--- keras.R
+++ keras.R
@@ -1,18 +1,18 @@
 # 케라스에 내장된 MNIST 데이터셋을 가져오기
-```
+
 library(keras)
 mnist <- dataset_mnist()
 train_images <-mnist$train$x
 train_images <-mnist$train$y
 test_images <- mnist$test$x
 test_images <- mnist$test$y
-```
 
+```
 >str(train_images)
 >str(train_labels)
 >str(test_images)
 >str(test_labels)
-
+```
 # 신경망 아키텍쳐
 network <- keras_model_sequential() %>%
   layer_dense(units = 512, activation="relu", input_shape = c(28*28)) %>%
@@ -37,3 +37,10 @@ test_images <- test_images/255
 tarin_label <- to_categorical(train_labels)
 test_labels <- to_categorical(test_labels)
 
+```
+>network %>% fit(train_images, train_labels, epochs =5, batch_size =128)
+```
+```
+>metrics <- network %>% evaluate(test_images, test_labels)
+>metrics
+```
